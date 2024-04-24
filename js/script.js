c5878e4951a30e0aac6f5a5a4944c4dc9c6026c1
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-04-24 18:22:51 +0000

    all files

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  const buttonOnChecked = document.getElementById("Positive-Option").checked
+  const positiveOption = Math.floor(Math.random() * 6) + 1
+  const negativeOption = Math.floor(Math.random() * -6) + 0
+
+  if (buttonOnChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+      "Your positive number is: " + positiveOption
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+      "Your negative number is: " + negativeOption
+  }
+}
